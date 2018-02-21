c6d1abd233c00b769675381fc28ca300a2a8d030
Roger <lakersli@gmail.com>
2018-02-21 04:51:03 -0800

    Declare variables

--- app/adapter/adapter.rb
+++ app/adapter/adapter.rb
@@ -6,7 +6,7 @@ module Adapter
  +  base_uri "eventbriteapi.com/v3/events/"
  +
  +  def event_search(keyword, location, start_date = DateTime.now.midnight)
- +    HTTParty.get('search', query: { token: "#{EB_TOKEN}",
+ +    @events = HTTParty.get('search', query: { token: "#{EB_TOKEN}",
                             q: keyword,
  +                          "location.address".to_sym => location,
  +                          "start_date.range_start".to_sym => start_date.to_s[0..-7],
@@ -15,7 +15,7 @@ module Adapter
 
     def price(event_id)
       # finds the first (cheapest) ticket class that is available for purchase
-      HTTParty.get("#{event_id}/ticket_classes")["ticket_classes"].find { |ticket_class| ticket_class["on_sale_status"] == "AVAILABLE" }
+      ticket_info = HTTParty.get("#{event_id}/ticket_classes")["ticket_classes"].find { |ticket_class| ticket_class["on_sale_status"] == "AVAILABLE" }
       price = 0
       if ticket_info["free"] # checks for presence of "free" key
         return price
