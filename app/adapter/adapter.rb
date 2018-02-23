c94da9234454a2734b465b4197ba1652f1bae84f
Roger <lakersli@gmail.com>
2018-02-23 03:40:38 -0800

    Handle edge case where ticket info for an event does not exist

--- app/adapter/adapter.rb
+++ app/adapter/adapter.rb
@@ -59,7 +59,7 @@ module Adapter
       # finds the first (cheapest) ticket class that is available for purchase
       ticket_info = HTTParty.get("#{event_id}/ticket_classes")["ticket_classes"].find { |ticket_class| ticket_class["on_sale_status"] == "AVAILABLE" }
       price = 0
-      if ticket_info["free"] # checks for presence of "free" key
+      if !ticket_info || ticket_info["free"] # if ticket info does not exist, or if there exists a "free" key within ticket info response 
         return price
       else
         cost = ticket_info["cost"]["display"]
