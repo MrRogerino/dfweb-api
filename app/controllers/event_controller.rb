ff96a5b5c7d81904e9ced84e87abc12f59d43843
Roger <lakersli@gmail.com>
2018-02-23 08:30:22 -0800

    Fix #ticket_price method to account for donation type tickets

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,17 +1,18 @@
 class EventController < ApplicationController
+  README_LINK = "https://github.com/MrRogerino/dfweb-api/blob/master/README.md"
   def welcome
-    render json: {instructions: "Welcome to the Down For Whatever (DFW) API. Please reference the documentation found here: https://github.com/MrRogerino/dfweb-api/blob/master/README.md, to help you make a decision on what fun events are going on in your area!", readme: "https://github.com/MrRogerino/dfweb-api/blob/master/README.md"}
+    render json: {instructions: "Welcome to the Down For Whatever (DFW) API. Please reference the documentation found here: #{README_LINK}, to help you make a decision on what fun events are going on in your area!", readme: README_LINK}
   end
 
   def whatever
     location = params[:location]
-    keyword = params[:q]
+    keyword = params[:q] ||= ""
     render json: {random_event: eventbrite_adapter.random_event(keyword, location)}
   end
 
   def itinerary
     location = params[:location]
-    keyword = params[:q]
+    keyword = params[:q] ||= ""
     start_date = params[:start_date]
     end_date = params[:end_date]
     events_per_day = params[:events_per_day]
